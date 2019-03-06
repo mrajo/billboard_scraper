0d718c02280831ae401666a0898e02c29c89769f
mrajo <anthony.c@stle.co>
2019-03-05 22:11:37 -0800

    scrapes year links, scrapes songs from each list, saves to sqlite db

---
+++ db.rb
@@ -0,0 +1,25 @@
+require 'sqlite3'
+
+DATABASE = "billboard.db"
+
+class BillboardDb
+    def initialize
+        @db = SQLite3::Database.new DATABASE
+        createTable
+    end
+
+    def createTable
+        @db.execute <<~SQL
+            create table if not exists Charts (
+                year int,
+                rank int,
+                song varchar(100),
+                artist varchar(100)
+            );
+        SQL
+    end
+
+    def addSong(year, rank, song, artist)
+        @db.execute "insert into Charts values (?, ?, ?, ?)", [ year, rank, song, artist]
+    end
+end
--- scrape.rb
+++ scrape.rb
@@ -1,12 +1,42 @@
+#!/usr/bin/env ruby
+
 require 'nokogiri'
 require 'open-uri'
+require './db.rb'
 
-doc = Nokogiri::HTML(open('https://en.wikipedia.org/wiki/Billboard_Year-End_Hot_100_singles_of_1991'))
+ROOT_URL = 'https://en.wikipedia.org'
+START_PATH = '/wiki/Billboard_year-end_top_30_singles_of_1950'
 
-doc.search('.wikitable tbody tr').each do |row|
-  num = row.search('th').text.strip
-  title = row.search('td:first a').text.strip
-  artist = row.search('td:last a').text.strip
+toc = Nokogiri::HTML(open("#{ROOT_URL}#{START_PATH}"))
+db = BillboardDb.new
 
-  puts "#{num}: #{artist} - #{title}"
+# get links to all year-end lists
+toc.search('//div[@aria-labelledby="Billboard_Year-End_Hot_100_singles"]//td//ul//li//a').each do |yearLink|
+  year = yearLink.text.strip
+
+  if !yearLink['href']
+    doc = toc
+  else
+    path = yearLink['href'].strip
+    doc = Nokogiri::HTML(open("#{ROOT_URL}#{path}"))
+  end
+
+  # table formats change in 1982
+  if (year.to_i < 1982)
+    doc.search('//table[contains(@class, "wikitable")]//tr[not(th)]').each do |row|
+      rank = row.search('.//td[1] | .//th[1]').text.strip
+      song = row.search('.//td[2]//a').text.strip
+      artist = row.search('.//td[3]//a').text.strip
+      db.addSong year, rank, song, artist
+      puts "Added song [#{year}] (#{rank}) #{artist} - #{song}"
+    end
+  else
+    doc.search('//table[contains(@class, "wikitable")]//tr[th[not(@scope="col")]]').each do |row|
+      rank = row.search('th').text.strip
+      song = row.search('td:first a').text.strip
+      artist = row.search('td:last a').text.strip
+      db.addSong year, rank, song, artist
+      puts "Added song [#{year}] (#{rank}) #{artist} - #{song}"
+    end
+  end
 end
